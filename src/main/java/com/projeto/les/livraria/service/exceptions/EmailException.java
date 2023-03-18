0461c6b9179bd20da2ece3fbce8266c482b7bdf7
Henrique Oliveira <henrit515@gmail.com>
2023-03-18 15:57:32 -0300

    Ajuste criar endpoint usuario

---
+++ src/main/java/com/projeto/les/livraria/service/exceptions/EmailException.java
@@ -0,0 +1,7 @@
+package com.projeto.les.livraria.service.exceptions;
+
+public class EmailException extends RuntimeException {
+    public EmailException(String msg) {
+        super(msg);
+    }
+}
